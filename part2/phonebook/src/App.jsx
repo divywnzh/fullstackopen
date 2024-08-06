3522061bb0f74e87186e3ecaf1673624238b243e
Divyansh Bhagat <divyanshbhgt@gmail.com>
2024-08-06 23:15:16 +0530

    2.14

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import personService from './services/persons'
 
 import Numbers from './components/Numbers'
 import PersonForm from './components/PersonForm'
@@ -14,15 +15,16 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
+    document.title="Phonebook"
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
+    personService
+    .getAll()
+      .then(initialPersons => {
         console.log('promise fulfilled')
-        setPersons(response.data)
+        setPersons(initialPersons)
       })
   }, [])
-  console.log('render', persons.length, 'notes')
+  console.log('render', persons.length, 'persons')
 
   const handleSearch=(event)=>{
     setSearchName(event.target.value.toLowerCase())
@@ -34,21 +36,34 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const DeleteContact = (id,name) => {
+    if (window.confirm(`Delete ${name} ?`))
+      personService
+        .remove(id)
+        .then(removedPerson => {
+          console.log(`deleted ${response.name}`)
+          setPersons(persons.filter(person => person.id!==removedPerson.id))
+        })
+  }
+
   const addContact = (event) => {
     event.preventDefault()
-    const noteObject = {
+    const personObject = {
       name: newName,
       number: newNumber,
-      id: String(persons.length + 1)
     }
 
-   if(persons.some(person => JSON.stringify(person.number) === JSON.stringify(noteObject.number))){
+   if(persons.some(person => JSON.stringify(person.number) === JSON.stringify(personObject.number))){
       alert(`The number ${newNumber} already exists in phonebook`)
    }else{
-    console.log(newName)
-    setPersons(persons.concat(noteObject))
-    setNewName('')
-    setNewNumber('')
+    personService
+      .create(personObject)
+        .then(returnedPerson=>{
+          setPersons(persons.concat(returnedPerson))
+          console.log(`added ${returnedPerson.name} contact ${returnedPerson.number}`)
+          setNewName('')
+          setNewNumber('')
+        })
    } 
   }
 
@@ -63,8 +78,8 @@ const App = () => {
         <Filter searchName={searchName} handleSearch={handleSearch} showAll={showAll} setShowAll={setShowAll} />
       <h2>Add New</h2>
         <PersonForm newName={newName} newNumber={newNumber} handleName={handleName} handleNumber={handleNumber} addContact={addContact} />
-      <h2>Numbers</h2>
-        <Numbers peopleToShow={peopleToShow} />
+      <h2>Numbers</h2> {/* direct passing here but call by reference in Numbers {()=>{handleDelete}} */}
+        <Numbers peopleToShow={peopleToShow} handleDelete={DeleteContact} /> 
       ...
     </div>
   )
--- part2/phonebook/src/components/Numbers.jsx
+++ part2/phonebook/src/components/Numbers.jsx
@@ -1,9 +1,12 @@
-const Numbers=({peopleToShow})=>{
+const Numbers=({peopleToShow,handleDelete})=>{
     return(
         <>
-        <div>
-            {peopleToShow.map((person)=><li key={person.id}>{person.name} {person.number}</li>)}
-        </div>
+        {peopleToShow.map((person)=>
+            <li key={person.id}>
+                {person.name} {person.number} &nbsp;
+                <button onClick={()=>handleDelete(person.id,person.name)}>delete</button>
+            </li>
+        )}
         </>
     )
 }
