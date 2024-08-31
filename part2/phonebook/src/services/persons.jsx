8f65c0fd57e11403460f14663c003137d9cec823
Divyansh Bhagat <divyanshbhgt@gmail.com>
2024-08-31 15:20:52 +0530

    proxy config for backend

--- part2/phonebook/src/services/persons.jsx
+++ part2/phonebook/src/services/persons.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl='http://localhost:3001/api/persons' //connected with backend
+const baseUrl='/api/persons' //connected with backend
 
 const getAll=()=>{
     const request=axios.get(baseUrl)
